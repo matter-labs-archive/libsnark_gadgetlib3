0c1d57a8ff57e6a71cb6c2c46a1684921ad3700b
K <kopanarin@yandex.ru>
2018-09-09 01:35:58 +0300

    battleships without hashes

---
+++ cmake-build-debug/libsnark/CMakeFiles/battleship.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/k/CLionProjects/libsnark/libsnark/gadgetlib3/tests/battleship.cpp" "/home/k/CLionProjects/libsnark/cmake-build-debug/libsnark/CMakeFiles/battleship.dir/gadgetlib3/tests/battleship.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BN_SUPPORT_SNARK=1"
+  "CURVE_BN128"
+  "DEBUG=1"
+  "NO_PT_COMPRESSION=1"
+  "USE_ASM"
+  "USE_MIXED_ADDITION=1"
+  "_GLIBCXX_DEBUG"
+  "_GLIBCXX_DEBUG_PEDANTIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../depends/libff"
+  "../depends/libfqfft"
+  "../depends/libff/libff/.."
+  "/usr/include/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/k/CLionProjects/libsnark/cmake-build-debug/libsnark/CMakeFiles/snark.dir/DependInfo.cmake"
+  "/home/k/CLionProjects/libsnark/cmake-build-debug/depends/libff/libff/CMakeFiles/ff.dir/DependInfo.cmake"
+  "/home/k/CLionProjects/libsnark/cmake-build-debug/depends/CMakeFiles/zm.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/libsnark/CMakeFiles/battleship.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/battleship.dir/gadgetlib3/tests/battleship.cpp.o"
+  "battleship.pdb"
+  "battleship"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
